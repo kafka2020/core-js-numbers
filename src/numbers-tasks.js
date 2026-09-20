6ba95dc90fb6a6841897be8b8205757be0b0a3b6
Andrew S. <shinkarev89@gmail.com>
2026-09-21 00:00:26 +0300

    upd: 15 tasks solved

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -202,8 +202,14 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let divisor = 2; divisor * divisor <= n; divisor += 1) {
+    if (n % divisor === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -221,8 +227,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (!Number.isNaN(Number(value))) {
+    return Number(value);
+  }
+  return def;
 }
 
 /**
@@ -236,8 +245,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -253,8 +262,19 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) return 0;
+  if (index === 1) return 1;
+  let previous = 0;
+  let current = 1;
+  let num;
+  for (let i = 2; i <= index; i += 1) {
+    num = previous + current;
+    const a = current;
+    current = num;
+    previous = a;
+  }
+  return num;
 }
 
 /**
